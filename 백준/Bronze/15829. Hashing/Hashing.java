8ca66a93bd96fa9b271cd563f018378f190aae20
odls <138199219+Oodls@users.noreply.github.com>
2025-01-27 14:08:01 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 15860 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,68 @@
+
+/*
+
+입력
+첫 줄에는 문자열의 길이 L이 들어온다. 둘째 줄에는 영문 소문자로만 이루어진 문자열이 들어온다.
+
+입력으로 주어지는 문자열은 모두 알파벳 소문자로만 구성되어 있다.
+
+출력
+문제에서 주어진 해시함수와 입력으로 주어진 문자열을 사용해 계산한 해시 값을 정수로 출력한다.
+
+Small (50점)
+1 ≤ L ≤ 5
+Large (50점)
+1 ≤ L ≤ 50
+
+예제 입력
+5
+abcde
+
+예제 출력
+4739715
+
+
+ */
+
+
+import java.io.*;
+
+public class Main {
+    private static final int r = 31;
+    private static final int M = 1234567891;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int[] sequence = new int[n];
+
+        for (int i = 0; i < s.length(); i++) {
+            sequence[i] = getNum(s.charAt(i));
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(hashing(sequence) + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+    static int getNum(char c) {
+        return (int) c % 96;
+    }
+
+    static int hashing(int[] sequence) {
+        int currentR = 1;
+        int result = 0;
+        for (int i = 0; i < sequence.length; i++) {
+            if (i > 0) {
+                currentR *= r;
+            }
+            result += sequence[i] * currentR % M;
+        }
+
+        return result;
+    }
+
+
+}
