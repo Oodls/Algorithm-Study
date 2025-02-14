28e4c4f3caa89ec8c16f826c8e3d9617867d62a2
odls <138199219+Oodls@users.noreply.github.com>
2025-02-14 17:50:25 +0900

    Update 줄 세우기.java

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -11,7 +11,7 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         HashMap<Integer, Integer> lookUpMap = new HashMap<Integer, Integer>(); // O(1) 조회용
         
-        Queue<Integer> nodeList = new LinkedList<Integer>();
+        Queue<Integer> nodeList = new LinkedList<Integer>(); // 위상정렬 큐 선언
         
         ArrayList<ArrayList<Integer>> adjacencyList = new ArrayList<>();
         for (int i = 0; i <= n; i++){ // 인접리스트 초기화
@@ -33,19 +33,19 @@ public class Main {
 
         for (int i = 1; i <= n; i ++){
             if (lookUpMap.get(i) == 0){
-                nodeList.add(i); // 우선순위 큐에 초기 노드 삽입
+                nodeList.add(i); // 큐에 진입차수 0인 노드 삽입하고 시작
             }
         }
         
         for (int i = 0; i < n; i ++){
-            int v = nodeList.poll();
+            int v = nodeList.poll(); // 큐에 노드 하나 뽑음 (진입차수가 0인 노드)
             bw.write(String.valueOf(v) + " ");
             for (int j = 0; j < adjacencyList.get(v).size(); j++){
                 int num = adjacencyList.get(v).get(j);
                 int temp = lookUpMap.get(num);
-                lookUpMap.put(num, temp - 1);
-                if (temp - 1 == 0){
-                    nodeList.add(num);
+                lookUpMap.put(num, temp - 1); // 진입차수 감산
+                if (temp - 1 == 0){ // 진입차수가 0이 되면 큐에 삽입
+                    nodeList.add(num); 
                 }
             }
         }
@@ -53,12 +53,14 @@ public class Main {
     }
 }
 
-class Node {
-    int node;
-    int count;
+/*
+위상 정렬 사용
+진입 차수 배열을 사용해서 0이 되는 값을 찾기 위해 순회하는건 시간복잡도가 높아 다른 방법 구상
 
-    public Node(int node, int count) {
-        this.node = node;
-        this.count = count;
-    }
-}
+먼저 처음 하려 했던건, 조회용 hashmap과 정렬용 treemap을 활용해서 comparator로 진입차수 값에 따라 정렬하고자 하였는데,
+키값으로 정렬하지 않으면 잘 안되는 문제 발생
+
+따라서 우선순위 큐를 활용해서 계속 진입차수 값이 변경될 때마다 우선순위 큐에 넣어 정렬되도록 했지만, 이는 메모리 낭비가 심해 메모리 초과 발생
+
+마지막으로 map에 넣어두던 노드정보에서 진입 차수가 0이 될 때만 큐에 저장, 이를 통해 해결
+*/
